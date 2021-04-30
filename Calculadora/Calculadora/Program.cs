3240f59fb4db02d524513cd447179b3e488d49ad
Heitor Bardemaker A. Bisneto <hbarbisneto@gmail.com>
2021-04-30 04:04:03 -0300

    Calculadora

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string SoftwareName = "Calculadora C# Console";
+            string Version = "1.0";
+            string CopyrightName = "Heitor Bisneto.";
+            DateTime AnoAtual = DateTime.Today;
+
+            Console.WriteLine("Nome: " + SoftwareName);
+            Console.WriteLine("Versão: " + Version);
+            Console.WriteLine("Criado por: " + CopyrightName);
+
+            string StrAnoAtual = AnoAtual.ToString("yyyy");
+            int IntAnoAtual = Convert.ToInt32(StrAnoAtual);
+
+            if (IntAnoAtual == 2021)
+            {
+                Console.WriteLine("Copyright © " + IntAnoAtual + " | " + CopyrightName + " All rights reserved.");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Copyright © 2021 - " + IntAnoAtual + " | " + CopyrightName + " All rights reserved.");
+                Console.WriteLine();
+            }
+
+            try
+            {
+                Console.WriteLine(">> Digite o primeiro número: ");
+                var N1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine();
+
+                try
+                {
+                    Console.WriteLine("---------- Lista de operações ----------");
+                    Console.WriteLine("Potência: **");
+                    Console.WriteLine("Raiz Quadrada: R ou r");
+                    Console.WriteLine("Multiplicação: *");
+                    Console.WriteLine("Divisão: /");
+                    Console.WriteLine("Adição: +");
+                    Console.WriteLine("Subtração: -");
+                    Console.WriteLine("---------- Lista de operações ----------");
+
+
+                    Console.WriteLine();
+                    Console.WriteLine(">> Digite o simbolo da operação de acordo com o exemplo acima: ");
+                    string Operacao = Console.ReadLine();
+
+                    if (Operacao == "R" || Operacao == "r")
+                    {
+                        double Calc = (N1*N1)/N1;
+                        double Calculo = Math.Pow(N1, (1 / 2d));
+
+                        Console.WriteLine(">> A Raiz Quadrada de " + N1 + " é " + Calculo);
+                    }
+                    else
+                    {
+                        try
+                        {
+                            Console.WriteLine(">> Insira o segundo número: ");
+                            var N2 = Convert.ToDouble(Console.ReadLine());
+
+                            if (Operacao == "+")
+                            {
+                                double Calculo = N1 + N2;
+                                Console.WriteLine(">> Resultado da adição: " + Calculo);
+                            }
+                            else if (Operacao == "-")
+                            {
+                                double Calculo = N1 - N2;
+                                Console.WriteLine(">> Resultado da Subtração: " + Calculo);
+                            }
+                            else if (Operacao == "*")
+                            {
+                                double Calculo = N1 * N2;
+                                Console.WriteLine(">> Resultado da Multiplicação: " + Calculo);
+                            }
+                            else if (Operacao == "/")
+                            {
+                                double Calculo = N1 / N2;
+                                Console.WriteLine(">> Resultado da Divisão: " + Calculo);
+                            }
+                            else if (Operacao == "**")
+                            {
+                                double Calculo = Math.Pow(N1, N2);
+                                Console.WriteLine(">> Resultado da Potência: " + Calculo);
+                            }
+
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine(">> SEGUNDO CARACTERE INCORRETO:");
+                            Console.WriteLine("> O segundo caractere digitado não é um número. Verifique o digito inserido e execute o programa novamente");
+                            Console.WriteLine();
+                            Console.WriteLine("Codigo do erro: " + ex);
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(">> OPERADOR INVÁLIDO:");
+                    Console.WriteLine("> Operação digitada incorretamente");
+                    Console.WriteLine();
+                    Console.WriteLine("Codigo do erro: " + ex);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.WriteLine(">> PRIMEIRO CARACTERE INCORRETO:");
+                Console.WriteLine("> O primeiro caractere digitado não é um número. Verifique o digito inserido e execute o programa novamente");
+                Console.WriteLine();
+                Console.WriteLine("Codigo do erro: " + ex);
+            }
+        }
+    }
+}
---
+++ Calculadora/Readme.md
@@ -0,0 +1,139 @@
+# Calculadora (Console C#)
+O programa ***Calculadora***, realiza cálculos básicos e avançados de acordo com as operações disponíveis:
+
+* Adição;
+* Subtração;
+* Multiplicação;
+* Divisão;
+* Potenciação;
+* Radiciação;
+
+> O projeto **Calculadora (Console C#)** foi criado seguindo o padrão do projeto em Python.
+<br> - Para saber mais sobre o projeto Calculadora (em Python) [clique aqui](https://github.com/hbisneto/Python/tree/main/Calculadora "Calculadora (em Python)") para ser redirecionado.
+
+## Calculadora: DEMO
+
+Demonstração do programa em execução
+
+```
+Nome: CALCULADORA - DEMO
+Versão: 1.0
+Criado por: Heitor Bisneto.
+Copyright © 2021 | Heitor Bisneto. All rights reserved.
+
+===================================
+========== Adição - Demo ==========
+===================================
+Insira o primeiro número: 9
+
+---------- Lista de operações ----------
+Potência = **
+Raiz Quadrada = R ou r
+Multiplicação = *
+Divisão = /
+Adição = +
+Subtração = -
+---------- Lista de operações ----------
+
+Digite o simbolo da operação de acordo com o exemplo acima: +
+Insira o segundo número: 1
+
+Resultado da Adição: 10
+
+======================================
+========== Subtração - Demo ==========
+======================================
+Insira o primeiro número: 9
+
+---------- Lista de operações ----------
+Potência = **
+Raiz Quadrada = R ou r
+Multiplicação = *
+Divisão = /
+Adição = +
+Subtração = -
+---------- Lista de operações ----------
+
+Digite o simbolo da operação de acordo com o exemplo acima: -
+Insira o segundo número: 1
+
+Resultado da Subtração: 8
+
+==========================================
+========== Multiplicação - Demo ==========
+==========================================
+Insira o primeiro número: 9
+
+---------- Lista de operações ----------
+Potência = **
+Raiz Quadrada = R ou r
+Multiplicação = *
+Divisão = /
+Adição = +
+Subtração = -
+---------- Lista de operações ----------
+
+Digite o simbolo da operação de acordo com o exemplo acima: *
+Insira o segundo número: 1
+
+Resultado da Multiplicação: 9
+
+====================================
+========== Divisão - Demo ==========
+====================================
+Insira o primeiro número: 9
+
+---------- Lista de operações ----------
+Potência = **
+Raiz Quadrada = R ou r
+Multiplicação = *
+Divisão = /
+Adição = +
+Subtração = -
+---------- Lista de operações ----------
+
+Digite o simbolo da operação de acordo com o exemplo acima: /
+Insira o segundo número: 1
+
+Resultado da Divisão: 9.0
+
+========================================
+========== Potênciação - Demo ==========
+========================================
+Insira o primeiro número: 9
+
+---------- Lista de operações ----------
+Potência = **
+Raiz Quadrada = R ou r
+Multiplicação = *
+Divisão = /
+Adição = +
+Subtração = -
+---------- Lista de operações ----------
+
+Digite o simbolo da operação de acordo com o exemplo acima: R
+Insira o segundo número: 1
+
+Resultado da Potênciação: 9
+
+=======================================
+========== Radiciação - Demo ==========
+=======================================
+Insira o primeiro número: 9
+
+---------- Lista de operações ----------
+Potência = **
+Raiz Quadrada = R ou r
+Multiplicação = *
+Divisão = /
+Adição = +
+Subtração = -
+---------- Lista de operações ----------
+
+Digite o simbolo da operação de acordo com o exemplo acima: R
+
+A Raiz Quadrada de 9 é 3.0
+```
+#
+
+Copyright © 2021 Heitor Bisneto. All rights reserved.
