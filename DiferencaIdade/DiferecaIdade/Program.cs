33afa8e7c48a229e8eaf49c56cb128966bd70d9f
Heitor Bardemaker A. Bisneto <hbarbisneto@gmail.com>
2021-06-02 04:32:35 -0300

    DiferencaIdade

---
+++ DiferencaIdade/DiferecaIdade/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace DiferecaIdade
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var CopyrightClass = new Copyright();
+            CopyrightClass.Print();
+           
+            Console.WriteLine(">> Insira seu nome:");
+            string Nome = Console.ReadLine();
+            Console.WriteLine(">> Insira o nome da segunda pessoa:");
+            string NomeSegundaPessoa = Console.ReadLine();
+            Console.WriteLine(">> Em que ano você nasceu:");
+            int Nascimento = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(">> Em que ano a segunda pessoa nasceu:");
+            int NascimentoSegundaPessoa = Convert.ToInt32(Console.ReadLine());
+
+            int Calc = (Nascimento - NascimentoSegundaPessoa);
+            int ConterOperation = Calc * (-1);
+            
+            if (Nascimento < NascimentoSegundaPessoa)
+            {
+                if (ConterOperation == 1)
+                {
+                    Console.WriteLine(">> " + Nome + " é " + ConterOperation + " ano mais velho(a) que " + NomeSegundaPessoa);
+                }
+                else
+                {
+                    Console.WriteLine(">> " + Nome + " é " + ConterOperation + " anos mais velho(a) que " + NomeSegundaPessoa);
+                }
+            }
+            else if (Nascimento > NascimentoSegundaPessoa)
+            {
+                if(ConterOperation == 1)
+                {
+                    Console.WriteLine(">> " + NomeSegundaPessoa + " é " + Calc + " ano mais velho(a) que " + Nome);
+                }
+                else
+                {
+                    Console.WriteLine(">> " + NomeSegundaPessoa + " é " + Calc + " anos mais velho(a) que " + Nome);
+                }
+            }
+            else
+            {
+                Console.WriteLine(">> Vocês têm a mesma idade!");
+            }
+        }
+    }
+}
---
+++ DiferencaIdade/README.md
@@ -0,0 +1,9 @@
+# DiferencaIdade
+O programa compara a data de nascimento de duas pessoas e informa:
+
+- Se uma pessoa é mais velha que a outra ou se as duas têm a mesma idade
+- A diferença de idade entre as pessoas
+
+#
+
+Copyright © 2021 Heitor Bisneto. All rights reserved.
