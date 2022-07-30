b1429cc4d6fc5ddaee395417a3f5f5b993cb2392
Heitor Bardemaker A. Bisneto <hbarbisneto@gmail.com>
2022-07-30 01:51:09 -0300

    Add files via upload

---
+++ KeyGenerator/AccessKey.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeyGenerator
+{
+    class AccessKey
+    {
+        // 8 
+        // 4
+        // 4
+        // 4
+        // 12
+        public int CreationPhase { get; set; }
+        public int PasswordSize { get; set; }
+        public string PassWordSplit1 { get; set; }
+        public string PassWordSplit2 { get; set; }
+        public string PassWordSplit3 { get; set; }
+        public string PassWordSplit4 { get; set; }
+        public string PassWordSplit5 { get; set; }
+        public string RandomPass { get; set; }
+        public void CreateKey()
+        {
+            // Create a string of characters, numbers, special characters that allowed in the password
+            //PasswordSize = 15;
+            string validChars = "ABCDEFGHJKLMNOPQRSTUVWXYZ0123456789";
+            Random random = new Random();
+
+            // Select one random character at a time from the string  
+            // and create an array of chars  
+            char[] chars = new char[PasswordSize];
+            for (int i = 0; i < PasswordSize; i++)
+            {
+                chars[i] = validChars[random.Next(0, validChars.Length)];
+            }
+            RandomPass = new string(chars);
+        }
+    }
+}
---
+++ KeyGenerator/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace KeyGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Keys = new AccessKey();
+
+            for(int i = 0; i < 5; i++)
+            {
+                Keys.CreationPhase++;
+                
+                if(Keys.CreationPhase == 1)
+                {
+                    Keys.PasswordSize = 8;
+                    Keys.CreateKey();
+                    Keys.PassWordSplit1 = Keys.RandomPass;
+                }
+
+                if (Keys.CreationPhase == 2)
+                {
+                    Keys.PasswordSize = 4;
+                    Keys.CreateKey();
+                    Keys.PassWordSplit2 = Keys.RandomPass;
+                }
+
+                if (Keys.CreationPhase == 3)
+                {
+                    Keys.PasswordSize = 4;
+                    Keys.CreateKey();
+                    Keys.PassWordSplit3 = Keys.RandomPass;
+                }
+
+                if (Keys.CreationPhase == 4)
+                {
+                    Keys.PasswordSize = 4;
+                    Keys.CreateKey();
+                    Keys.PassWordSplit4 = Keys.RandomPass;
+                }
+
+                if (Keys.CreationPhase == 5)
+                {
+                    Keys.PasswordSize = 12;
+                    Keys.CreateKey();
+                    Keys.PassWordSplit5 = Keys.RandomPass;
+                }
+            }
+            Console.WriteLine($"{Keys.PassWordSplit1}-{Keys.PassWordSplit2}-{Keys.PassWordSplit3}-{Keys.PassWordSplit4}-{Keys.PassWordSplit5}");
+
+            // 8
+            // 4
+            // 4
+            // 4
+            // 12
+            //WOML2WAS-G0Q7-58FH-JTBO-YF03ZWEO7B00
+            //S4QBRXOD-WYVG-8DK1-92VP-NNL70VYNQ3KA
+            //R71Y0JXD-0X1G-KLGJ-W2CA-XFPH3FQAWW2C
+
+            //R71Y0JXD-0X1G-KLGJ-XFPH3FQA
+        }
+    }
+}
